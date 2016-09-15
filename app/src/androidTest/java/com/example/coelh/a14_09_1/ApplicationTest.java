02fb6eb140762b30fe82a3c9483a21992c7edcdb
luis15 <coelho.luisantonio@hotmail.com>
2016-09-14 21:21:57 -0300

    Front-end

---
+++ app/src/androidTest/java/com/example/coelh/a14_09_1/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.example.coelh.a14_09_1;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
